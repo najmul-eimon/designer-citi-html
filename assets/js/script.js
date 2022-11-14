11df8c90ca8b12c633e7b088ecfdbeeb0dca2cfc
najmul-eimon <najmul.eimon@intelli.global>
2022-11-14 11:37:07 +0600

    slider update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(function () {
   "use strict";
 
   /*======================== related-product-slider ===========================*/
-  var swiper = new Swiper(".related-product-slider", {
+  var swiperRelated = new Swiper(".related-product-slider", {
     spaceBetween: 20,
     loop: true,
     autoplay: true,
@@ -24,11 +24,11 @@ $(function () {
   });
 
   $(".related-product-slider").mouseenter(function(){
-    swiper.autoplay.stop();
+    swiperRelated.autoplay.stop();
   });
 
   $(".related-product-slider").mouseleave(function(){
-    swiper.autoplay.start();
+    swiperRelated.autoplay.start();
   });
 
   /*======================== mobile menu and bottom navigation ===========================*/
@@ -82,7 +82,7 @@ $(function () {
 
   /*============================= banner slider ==============================*/
 
-  var swiper = new Swiper(".banner-slide", {
+  var swiperBanner = new Swiper(".banner-slide", {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
@@ -111,12 +111,11 @@ $(function () {
   /*========================== product detail modal ===========================*/
   $(".product-image").click(function () {
     $("#product_modal").modal("show");
-    console.log('clicked');
   });
   
 
   /*============================ product detail description slider ==============================*/
-  var swiper = new Swiper(".product-desc-slider", {
+  var swiperProdDesc = new Swiper(".product-desc-slider", {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
@@ -154,32 +153,31 @@ $(function () {
       $(this).parent().parent().parent().find(".static_btn").css({ display: "block" });
     }
   });
-});
 
-/*=========================== product modal slider =============================*/
-// input spinner
-$(".plus-btn").click(function () {
-  let $current_val = parseInt($(this).siblings("input").val());
+  /*=========================== added cart counter  =============================*/
+  // input spinner
+  $(".plus-btn").click(function () {
+    let $current_val = parseInt($(this).siblings("input").val());
 
-  let $update_val = $current_val + 1;
-  $(this).siblings("input").val($update_val);
-});
-$(".minus-btn").click(function () {
-  let $current_val = parseInt($(this).siblings("input").val());
-  let $update_val;
-
-  if($current_val > 0){
-    $update_val = $current_val - 1;
+    let $update_val = $current_val + 1;
     $(this).siblings("input").val($update_val);
-  }
+  });
+  $(".minus-btn").click(function () {
+    let $current_val = parseInt($(this).siblings("input").val());
+    let $update_val;
+
+    if($current_val > 0){
+      $update_val = $current_val - 1;
+      $(this).siblings("input").val($update_val);
+    }
+  });
+
 });
 
 /*=========================== product modal slider =============================*/
 const multipleSwiperSlides = function () {
-  let sliderMain = document.querySelectorAll(
-    ".swiper-container.js-slider--main"
-  );
-  let sliderNav = document.querySelectorAll(".swiper-container.js-slider--nav");
+  let sliderMain = document.querySelectorAll(".swiper.js-slider--main");
+  let sliderNav = document.querySelectorAll(".swiper.js-slider--nav");
 
   // Arrays to hold swiper instances
   let mainArray = [];
@@ -214,19 +212,14 @@ const multipleSwiperSlides = function () {
   const checkOnPage = function () {
     if (sliderMain.length > 0 && sliderNav.length > 0) {
       let numberOfSlides = mainArray.length || navArray.length || 0;
-
       if (mainArray.length !== navArray.length) {
-        console.warn(
-          "multipleSwiperSlides: Number of main slides and nav slides is different. Expect incorrect behaviour."
-        );
+        console.warn("multipleSwiperSlides: Number of main slides and nav slides is different. Expect incorrect behaviour.");
       }
-
       for (let i = 0; i < numberOfSlides; i++) {
         mainArray[i].controller.control = navArray[i];
         navArray[i].controller.control = mainArray[i];
       }
-
-      console.log("multipleSwiperSlides: Things should be working fine. B)");
+      // console.log("multipleSwiperSlides: Things should be working fine. B)");
     }
   };
 
